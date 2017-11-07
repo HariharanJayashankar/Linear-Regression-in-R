22eaaf983805440c427f3c5bbdf9149cd894a3ed
HariharanJayashankar <hariharan.jayashankar@gmail.com>
2017-11-07 10:58:13 +0530

    Function to execute the basic gradient descent algorithm
    The function works really similary to the for loop in the other script.

---
+++ Gradient_Desc_Linear_Regression.R
@@ -0,0 +1,32 @@
+#Trying to create the code for gradient descent
+rm(list = ls())
+
+x0 <- rep.int(1, 50)
+x1 <- sample(100:1000, 50)
+Y <- as.matrix(rnorm(50, 2, 4))
+
+X <- as.matrix(cbind(x0, x1))
+
+B <- matrix(, nrow = 2, ncol = 50)
+
+X.scaled <- X
+X.scaled[, 2] <- (X[,2] - mean(X[,2]))/sd(X[,2])
+
+
+grad.desc <- function(x, y, alpha = 0.5, k = 1000) {
+        
+        m <- nrow(x)
+        
+        for (i in 1:k) {
+                B <- matrix( c(0, 0), nrow = 2, ncol = 1)
+                
+                B <- B - (alpha)*(((1/m) * 2 * t(x)) %*% (y - ((x)%*%B)))
+                
+                if (identical ((y - (x %*% B)), as.matrix( rep(50), nrow = 50, ncol = 1))) {
+                        break    
+                }
+                
+                
+        }
+        return(B)
+}
